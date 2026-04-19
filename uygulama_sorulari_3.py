291dda966232d40132f09c364d6334f5c1d32ea4
Faruk Akbayrak <farukakbayrak0@gmail.com>
2026-04-19 11:18:43 +0300

    Üçüncü commit

---
+++ uygulama_sorulari_3.py
@@ -0,0 +1,48 @@
+# 1) def islem_merkezi(sayi1, sayi2):
+#     def toplama():
+#         return sayi1 + sayi2
+#     def cikarma():
+#         return sayi1 - sayi2
+#     def bolme():
+#         return sayi1 / sayi2
+    
+
+#     dict = {
+#         "toplam": toplama(),
+#         "cikarm": cikarma(),
+#         "bolme": bolme()}
+    
+#     print(dict)
+
+# islem_merkezi(10, 5)
+
+
+# 2) def logger(seviye):
+#     def mesaj_yaz(metin):
+#         if seviye =="INFO":
+#             return f"{seviye} {metin}"
+#         elif seviye =="ERROR": 
+#             return f"{seviye} {metin}"
+#     return mesaj_yaz 
+    
+# bilgi_log = logger("INFO")
+# hata_log = logger("ERROR")
+# print(bilgi_log("Sistem çalışıyor"))
+# print(hata_log("Bağlantı hatası"))
+
+
+# 3) def hesap_makinesi(liste,islem_fonksiyonu):
+#     sonuc=[]
+#     for i in liste:
+#         sonuc.append(islem_fonksiyonu(i))
+#     return sonuc
+
+# def kare_al(x):
+#     return x**2
+
+# def kup_al(x):
+#     return x**3
+
+
+# print(hesap_makinesi([1,2,3,4,5],kare_al))
+# print(hesap_makinesi([1,2,3,4,5],kup_al))
