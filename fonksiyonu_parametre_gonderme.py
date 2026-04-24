d178586df0f40cd5af7fc607aa5e037ab64bbb94
fakbayrak <farukakbayrak0@gmail.com>
2026-04-25 01:15:01 +0300

    Yerel degisiklikler kaydedildi

---
+++ fonksiyonu_parametre_gonderme.py
@@ -0,0 +1,32 @@
+# def topla(a,b): 
+#     return a + b
+
+# def carp(a,b):
+#     return a * b
+#
+#  def hesapla(a,b,fonksiyon):
+#     return fonksiyon(a,b)
+
+
+# print(hesapla(3,4,topla))   
+# print(hesapla(3,4,carp))
+
+
+
+
+# def kare_al(x):
+#     return x**2
+
+# def kup_al(x):
+#     return x**3
+
+# def listeyi_isle(liste,fonksiyon):
+#     sonuc=[]
+#     for i in liste:
+#         sonuc.append(fonksiyon(i))
+#     return sonuc    
+
+# sayilar=[1,2,3,4,5]
+# print(listeyi_isle(sayilar,kare_al))
+# print(listeyi_isle(sayilar,kup_al))
+
